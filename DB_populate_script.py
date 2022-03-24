3fd09fb8ffa425069ae3f66fe1f86d9fa1fcffa9
Alexandre Mathieu <alexandre.mathieu.7@ulaval.ca>
2022-03-23 21:32:25 -0400

    Add generate_subscribers and generate_sub_posts

--- DB_populate_script.py
+++ DB_populate_script.py
@@ -16,10 +16,12 @@ def create_DB():
     cursor.execute("DROP DATABASE IF EXISTS DitRed;")
     cursor.execute("CREATE DATABASE DitRed;")
 
+
 def create_tables(cursor):
     commands = parse_SQL_script("DB_creation.sql")
     for command in commands:
         cursor.execute(command)
+
 
 def parse_SQL_script(sql_file_path):
     with open(sql_file_path, 'r', encoding='utf-8') as f:
@@ -36,36 +38,53 @@ def parse_SQL_script(sql_file_path):
                 stmt = ''
     return stmts
 
+
 def get_users_count(cursor):
-    selectRequest = "SELECT COUNT(*) FROM Users;"
-    cursor.execute(selectRequest)
-    usersCount = cursor.fetchone()[0]
-    return usersCount
+    select_request = "SELECT COUNT(*) FROM Users;"
+    cursor.execute(select_request)
+    users_count = cursor.fetchone()[0]
+    return users_count
+
+
+def get_sub_timestamp(cursor, sub_id):
+    select_request = f"""SELECT timestamp FROM Subs WHERE id = {sub_id};"""
+    cursor.execute(select_request)
+    timestamp = cursor.fetchone()[0]
+    return timestamp
+
 
 def generate_timestamp_after(other_timestamp):
-    year = randint(int(other_timestamp[:4]), 2022)
-    month = randint(int(other_timestamp[5:7]), 12)
-    day = randint(int(other_timestamp[8:10]), 28)
-    hour = randint(int(other_timestamp[11:13]), 23)
-    minute = randint(int(other_timestamp[14:16]), 59)
-    sec = randint(int(other_timestamp[17:19]), 59)
+    year = randint(other_timestamp.year, 2022)
+    month = randint(other_timestamp.month, 12)
+    day = randint(other_timestamp.day, 28)
+    hour = randint(other_timestamp.hour, 23)
+    minute = randint(other_timestamp.minute, 59)
+    sec = randint(other_timestamp.second, 59)
 
-    hour = 11 if month == 3 else hour #winter/summer time bug in mySQL
+    hour = 11 if month == 3 else hour  # winter/summer time bug in mySQL
     timestamp = datetime.datetime(year, month, day, hour, minute, sec)
 
     return str(timestamp)[:19]
 
-def generate_random_timestamp():
-    return generate_timestamp_after("2004-01-01 00:00:00")
 
-def generate_users(cursor, usersNumber):
-    for username in generate_username(usersNumber):
+def generate_random_timestamp():
+    return generate_timestamp_after(datetime.datetime(2004, 1, 1))
+
+
+def generate_users(cursor, users_number):
+    user_names = set()
+    while len(user_names) < users_number:
+        user_names.add(generate_username(1)[0])
+    user_names = list(user_names)
+
+    for username in user_names:
         bio = get_sentence(count=(2, 5))
         age = randint(13, 100)
         request = f"""INSERT INTO Users (email, username, bio, age) VALUES ('{username}@mail.com', '{username[:30]}', '{bio}', {age});"""
         cursor.execute(request)
 
         generate_last_user_password(cursor)
+
 
 def generate_last_user_password(cursor):
     user_id = get_users_count(cursor)
@@ -75,55 +94,80 @@ def generate_last_user_password(cursor):
     request = f"""INSERT INTO Passwords (user_id, hashed_password) VALUES ({user_id}, '{crypted_password}');"""
     cursor.execute(request)
 
+
 def generate_wall_posts(cursor):
-    usersCount = get_users_count(cursor)
-    for user_id in range(1, usersCount + 1):
-        postsCount = randint(0, 5)
-        for _ in range(postsCount):
-            message = get_paragraph(count=(1,4), sentence_range=(2, 8))
+    users_count = get_users_count(cursor)
+    for user_id in range(1, users_count + 1):
+        posts_count = randint(0, 5)
+        for _ in range(posts_count):
+            message = get_paragraph(count=(1, 4), sentence_range=(2, 8))
             request = f"""INSERT INTO WallPosts (user_id, message, timestamp) VALUES """
             request += f"""({user_id}, '{message}', '{generate_random_timestamp()}');"""
             cursor.execute(request)
 
+
 def generate_private_chat_messages(cursor):
-    usersCount = get_users_count(cursor)
+    users_count = get_users_count(cursor)
     # starts at 6 because 0-5 discussions are created with people having lower ids than the current id 
-    for user_id in range(6, usersCount + 1):
-        discussionsCount = randint(0, 5)
-        for _ in range(discussionsCount):
+    for user_id in range(6, users_count + 1):
+        discussions_count = randint(0, 5)
+        for _ in range(discussions_count):
             friend_id = randint(1, user_id - 1)
-            messagesNumber = randint(5, 30)
+            messages_number = randint(5, 30)
             friends_ids = [user_id, friend_id]
-            for _ in range(messagesNumber):
+            for _ in range(messages_number):
                 sender = randint(0, 1)
                 message = get_sentence(count=(1, 10), word_range=(1, 20))
-                request = f"""INSERT INTO Messages (sender_id, receiver_id, timestamp, message) VALUES """ 
+                request = f"""INSERT INTO Messages (sender_id, receiver_id, timestamp, message) VALUES """
                 request += f"""({friends_ids[sender]}, {friends_ids[1 - sender]}, '{generate_random_timestamp()}', '{message}');"""
                 cursor.execute(request)
 
-def generate_subs(cursor, subsNumber):
-    usersNumber = get_users_count(cursor)
-    for _ in range(subsNumber):
-        sub_name = get_sentence(count=1, word_range=(1, 10))[:100].strip('. ')
-        user_id = randint(1, usersNumber)
+
+def generate_subs(cursor, subs_number):
+    users_number = get_users_count(cursor)
+
+    sub_names = set()
+    while len(sub_names) < subs_number:
+        sub_names.add(get_sentence(count=1, word_range=(1, 10))[:100].strip('. '))
+    sub_names = list(sub_names)
+
+    for i in range(subs_number):
+        sub_name = sub_names[i]
+        user_id = randint(1, users_number)
         descr = get_paragraph(count=1, sentence_range=(1, 4))
-        request = f"""INSERT INTO Subs (name, creator_id, timestamp, description, subscribers_count) VALUES """ 
+        request = f"""INSERT INTO Subs (name, creator_id, timestamp, description, subscribers_count) VALUES """
         request += f"""('{sub_name}', {user_id}, '{generate_random_timestamp()}', '{descr}', 0);"""
         cursor.execute(request)
 
-def generate_subscribers(cursor):
-    #INSERT INTO Subscribers (user_id, sub_id) VALUES ();
-    #TODO il faut faire la gâchette SQL pour incrémenter le nb de subscribers du Sub
-    pass
 
-def generate_sub_posts(cursor):
-    pass
+def generate_subscribers(cursor, subscribers_number, subs_number):
+    for subscriber_id in range(1, subscribers_number+1):
+        request = f"""INSERT INTO SUBSCRIBERS (user_id, sub_id) VALUES ({subscriber_id}, {(subscribers_number - subscriber_id) % subs_number});"""
+        cursor.execute(request)
+
+    # TODO il faut faire la gâchette SQL pour incrémenter le nb de subscribers du Sub
+
+
+def generate_sub_posts(cursor, subs_number, users_number):
+    for sub_id in range(1, subs_number+1):
+        creator_id = randint(1, users_number)
+        sub_timestamp = get_sub_timestamp(cursor, sub_id)
+        timestamp = generate_timestamp_after(sub_timestamp)
+        title = get_sentence(count=1, word_range=(1, 10))[:100].strip('. ')
+        content = get_sentence(count=randint(1, 5), word_range=(1, 10))
+
+        request = f"""INSERT INTO SUBPOSTS (sub_id, creator_id, timestamp, title, content, score, comments_count) """
+        request += f"""VALUES ({sub_id}, {creator_id}, '{timestamp}', '{title}', '{content}', 0, 0);"""
+        cursor.execute(request)
+
 
 def generate_posts_votes(cursor):
     pass
 
+
 def generate_sub_post_comments(cursor):
     pass
+
 
 def generate_sub_post_comments_votes(cursor):
     pass
@@ -143,13 +187,16 @@ if __name__ == '__main__':
 
     create_tables(cursor)
 
-    generate_users(cursor, 400)
+    users_number = 50
+    generate_users(cursor, users_number)
     generate_wall_posts(cursor)
     generate_private_chat_messages(cursor)
-    
-    generate_subs(cursor, 120)
-    generate_subscribers(cursor)
-    generate_sub_posts(cursor)
+
+    subs_number = 25
+    subscribers_number = 30
+    generate_subs(cursor, subs_number)
+    generate_subscribers(cursor, subscribers_number, subs_number)
+    generate_sub_posts(cursor, subs_number, users_number)
     generate_posts_votes(cursor)
     generate_sub_post_comments(cursor)
     generate_sub_post_comments_votes(cursor)
