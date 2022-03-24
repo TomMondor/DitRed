ca72094aee1030ef7a8b05fd8ce4c55dbb339b98
Thomas Mondor <thomas.mondor.1@ulaval.ca>
2022-03-23 22:37:34 -0400

    fix foreign keys syntax

--- DB_creation.sql
+++ DB_creation.sql
@@ -13,8 +13,9 @@ CREATE TABLE Users (
 );
 
 CREATE TABLE Passwords (
-    user_id INT UNSIGNED UNIQUE REFERENCES Users(id),
-    hashed_password CHAR(96) NOT NULL
+    user_id INT UNSIGNED UNIQUE,
+    hashed_password CHAR(96) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(id)
 );
 
 -- ---------------------
@@ -23,10 +24,12 @@ CREATE TABLE Passwords (
 
 CREATE TABLE Messages (
     id          BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    sender_id   INT UNSIGNED NOT NULL REFERENCES Users(id),
-    receiver_id INT UNSIGNED NOT NULL REFERENCES Users(id),
+    sender_id   INT UNSIGNED NOT NULL,
+    receiver_id INT UNSIGNED NOT NULL,
     timestamp   TIMESTAMP NOT NULL,
-    message     VARCHAR (3000) NOT NULL
+    message     VARCHAR (3000) NOT NULL,
+    FOREIGN KEY (sender_id) REFERENCES Users(id),
+    FOREIGN KEY (receiver_id) REFERENCES Users(id)
 );
 
 -- ---------------------
@@ -35,9 +38,10 @@ CREATE TABLE Messages (
 
 CREATE TABLE WallPosts (
     id        BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    user_id   INT UNSIGNED NOT NULL REFERENCES Users(id),
+    user_id   INT UNSIGNED NOT NULL,
     message   TEXT NOT NULL,
-    timestamp TIMESTAMP NOT NULL
+    timestamp TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(id)
 );
 
 -- ---------------------
@@ -47,52 +51,64 @@ CREATE TABLE WallPosts (
 CREATE TABLE Subs (
     id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL UNIQUE,
-    creator_id INT UNSIGNED NOT NULL REFERENCES Users(id),
+    creator_id INT UNSIGNED NOT NULL,
     timestamp TIMESTAMP NOT NULL,
     description VARCHAR(1000) NOT NULL,
-    subscribers_count INT UNSIGNED NOT NULL
+    subscribers_count INT UNSIGNED NOT NULL,
+    FOREIGN KEY (creator_id) REFERENCES Users(id)
 );
 
 CREATE TABLE Subscribers (
-    user_id INT UNSIGNED NOT NULL REFERENCES Users(id),
-    sub_id BIGINT UNSIGNED NOT NULL REFERENCES Subs(id),
-    PRIMARY KEY (user_id, sub_id)
+    user_id INT UNSIGNED NOT NULL,
+    sub_id BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (user_id, sub_id),
+    FOREIGN KEY (user_id) REFERENCES Users(id),
+    FOREIGN KEY (sub_id) REFERENCES Subs(id)
 );
 
 CREATE TABLE SubPosts (
     id          BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    sub_id      BIGINT UNSIGNED NOT NULL REFERENCES Subs(id),
-    creator_id  INT UNSIGNED NOT NULL REFERENCES Users(id),
+    sub_id      BIGINT UNSIGNED NOT NULL,
+    creator_id  INT UNSIGNED NOT NULL,
     timestamp   TIMESTAMP NOT NULL,
     title       VARCHAR(256) NOT NULL,
     content     TEXT NOT NULL,
     score       INT NOT NULL,
-    comments_count INT UNSIGNED NOT NULL
+    comments_count INT UNSIGNED NOT NULL,
+    FOREIGN KEY (sub_id) REFERENCES Subs(id),
+    FOREIGN KEY (creator_id) REFERENCES Users(id)
 );
 
 CREATE TABLE SubPostsVotes (
-    sub_post_id BIGINT UNSIGNED REFERENCES SubPosts(id),
-    user_id     INT UNSIGNED NOT NULL REFERENCES Users(id),
+    sub_post_id BIGINT UNSIGNED,
+    user_id     INT UNSIGNED NOT NULL,
     vote        ENUM('upvote', 'downvote'),
-    PRIMARY KEY (sub_post_id, user_id)
+    PRIMARY KEY (sub_post_id, user_id),
+    FOREIGN KEY (sub_post_id) REFERENCES SubPosts(id),
+    FOREIGN KEY (user_id) REFERENCES Users(id)
 );
 
 CREATE TABLE SubPostComments (
     id          BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    sub_post_id BIGINT UNSIGNED NOT NULL REFERENCES SubPosts(id),
-    user_id     INT UNSIGNED NOT NULL REFERENCES Users(id),
+    sub_post_id BIGINT UNSIGNED NOT NULL,
+    user_id     INT UNSIGNED NOT NULL,
     timestamp   TIMESTAMP NOT NULL,
     comment     VARCHAR(3000) NOT NULL,
     score       INT NOT NULL,
-    answered_comment_id BIGINT UNSIGNED REFERENCES SubPostComments(id)
+    answered_comment_id BIGINT UNSIGNED,
 -- null means this comment replies to the post itself
+    FOREIGN KEY (sub_post_id) REFERENCES SubPosts(id),
+    FOREIGN KEY (user_id) REFERENCES Users(id),
+    FOREIGN KEY (answered_comment_id) REFERENCES SubPostComments(id)
 );
 
 CREATE TABLE SubPostCommentsVotes (
-    sub_post_comment_id BIGINT UNSIGNED REFERENCES SubPostComments(id),
-    user_id             INT UNSIGNED NOT NULL REFERENCES Users(id),
+    sub_post_comment_id BIGINT UNSIGNED,
+    user_id             INT UNSIGNED NOT NULL,
     vote                ENUM('upvote', 'downvote'),
-    PRIMARY KEY (sub_post_comment_id, user_id)
+    PRIMARY KEY (sub_post_comment_id, user_id),
+    FOREIGN KEY (sub_post_comment_id) REFERENCES SubPostComments(id),
+    FOREIGN KEY (user_id) REFERENCES Users(id)
 );
 
 
@@ -108,7 +124,7 @@ BEGIN
     UPDATE Subs S
     SET S.subscribers_count = S.subscribers_count + 1
     WHERE S.id = NEW.sub_id;
-END;//
+END; //
 DELIMITER ;
 
 DELIMITER //
@@ -119,7 +135,7 @@ BEGIN
     UPDATE SubPosts S
     SET S.comments_count = S.comments_count + 1
     WHERE S.id = NEW.sub_post_id;
-END;//
+END; //
 DELIMITER ;
 
 
@@ -135,20 +151,9 @@ BEGIN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'The comment answers a comment on a different post';
     END IF;
-END;//
+END; //
 DELIMITER ;
 
 -- ---------------------
 -- INDEXES
 -- ---------------------
-
--- ---------------------
--- SEE THE DB
--- ---------------------
-SHOW databases;
-SHOW tables;
-SELECT * FROM Users;
-SELECT * FROM Passwords;
-SELECT * FROM WallPosts;
-SELECT * FROM Messages;
-SELECT * FROM Subs;
