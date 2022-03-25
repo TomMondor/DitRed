9f52a6cde0195ac41d5b6301eeb355f96b0a68c7
Thomas Mondor <thomas.mondor.1@ulaval.ca>
2022-03-24 22:19:01 -0400

    add first api function

--- model/app.py
+++ model/app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from repositories.users_repository import UsersRepository
+from assemblers.user_assembler import UserAssembler
 
 app = Flask(__name__)
+
+users_repository = UsersRepository()
+user_assembler = UserAssembler()
 
 
 @app.route("/")
@@ -10,7 +15,9 @@ def start_page():
 
 @app.route("/users", methods=["GET"])
 def get_all_users():
-    pass
+    users = users_repository.get_users()
+    response = jsonify(user_assembler.assemble_users(users))
+    return response
 
 
 @app.route("/users/:userId", methods=["GET"])
@@ -21,3 +28,7 @@ def get_user():
 @app.route("/users/:userId", methods=["POST"])
 def post_user():
     pass
+
+
+if __name__ == '__main__':
+    app.run()
