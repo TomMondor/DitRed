509c2a59362554b1b2aca137b53589b0f2c68f34
Alexandre Mathieu <alexandre.mathieu.7@ulaval.ca>
2022-03-24 20:52:04 -0400

    Fix router & add SubPostCard

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <sub-post-card
+            :subName="subName"
+            :postCreator="postCreator"
+            :timestamp="timestamp"
+            :title="title"
+            :content="content"
+            :score="score"
+            :comments_count="comments_count"
+        />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SubPostCard from "./components/SubPostCard.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {
+        SubPostCard,
+    },
+    data: () => {
+        return {
+            subName: "Memes",
+            postCreator: "Nocturnal",
+            timestamp: "2015-03-26 11:46:40",
+            title: "Tempor exercitation reprehenderit nostrud commodo aliquip nulla minim",
+            content:
+                "Nisi commodo veniam ad ex do, id commodo irure et sed minim deserunt sunt. Aute quis exercitation sed voluptate.",
+            score: "123",
+            comments_count: "8",
+        };
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    margin-top: 60px;
 }
 </style>
---
+++ vue/src/components/Score.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="score-container">
+        <font-awesome-icon icon="fa-solid fa-arrow-up" class="arrow arrow-up" />
+        <div class="score-text">{{ score }}</div>
+        <font-awesome-icon
+            icon="fa-solid fa-arrow-down"
+            class="arrow arrow-down"
+        />
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Score",
+    props: ["score"],
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.score-container {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    width: fit-content;
+    height: fit-content;
+    padding: 1rem;
+
+    justify-content: center;
+}
+
+.score-text {
+    color: white;
+}
+
+.arrow {
+    color: grey;
+    height: 1.5rem;
+    width: 1.5rem;
+}
+
+.arrow:hover {
+    cursor: pointer;
+}
+
+.arrow-down:hover {
+    color: darkblue;
+}
+
+.arrow-up:hover {
+    color: red;
+}
+</style>
