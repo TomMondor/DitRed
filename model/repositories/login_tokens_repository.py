8e594e8ffd65e631cba0172363dff658f55a819a
Alexandre Mathieu <alexandre.mathieu.7@ulaval.ca>
2022-03-31 16:27:07 -0400

    change list to dictionary to store login tokens

--- model/repositories/login_tokens_repository.py
+++ model/repositories/login_tokens_repository.py
@@ -1,10 +1,11 @@
 from uuid import uuid4
 
+
 class LoginTokensRepository():
     def __init__(self):
-        self.login_tokens = []
+        self.login_tokens = {}
 
-    def create_login_token(self, userId):
+    def create_login_token(self, user_id):
         login_token = uuid4()
-        self.login_tokens.append({userId: login_token})
+        self.login_tokens[user_id] = login_token
         return login_token
