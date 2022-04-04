0e06252d6bc055adf61634441f888d29440d223a
Thomas Mondor <thomas.mondor.1@ulaval.ca>
2022-04-04 18:03:45 -0400

    add Subs page and router-link to /sub/:subId pages

---
+++ vue/src/components/subs/SubCard.vue
@@ -0,0 +1,100 @@
+<template>
+	<div>
+		<div class="sub-container">
+			<div class="display-row">
+				<span class="sub-name" @click="redirectToSubPage"
+					>r/{{ subData.name }}</span
+				>
+				<span
+					class="subscribe-button"
+					v-if="userId != undefined"
+					@click="subscribe"
+					>Subscribe</span
+				>
+			</div>
+			<div class="sub-description">{{ subData.description }}</div>
+			<div class="display-row">
+				<div>{{ subData.subscribers_count }} subscribers</div>
+				<div>Created on {{ subData.timestamp.slice(5, 16) }}</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import { subscribe } from "../../api/subAPI";
+import Cookies from "js-cookie";
+
+export default {
+	name: "SubCard",
+	props: {
+		subData: Object,
+		subId: Number,
+	},
+	data: () => {
+		return {
+			userId: Cookies.get("userId"),
+		};
+	},
+	mounted() {},
+	methods: {
+		async subscribe() {
+			await subscribe(this.subId);
+			//TODO vérifier si fonctionne quand le call d'api sera fait dans le b-e
+		},
+		redirectToSubPage() {
+			this.$router.push("/sub/" + this.subId);
+		},
+	},
+};
+</script>
+
+<style scoped>
+.sub-container {
+	display: flex;
+	flex-direction: column;
+	width: 50vw;
+	height: fit-content;
+	padding: 1rem;
+	gap: 1rem;
+
+	background-color: #222222;
+	color: grey;
+	border-radius: 1rem;
+}
+.display-row {
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: flex-start;
+}
+.sub-name {
+	color: white;
+	font-size: 1.5rem;
+	cursor: pointer;
+}
+.subscribe-button {
+	padding: 0.5rem;
+	border-radius: 1rem;
+	background-color: orange;
+	color: black;
+	font-weight: bold;
+}
+.subscribe-button:hover {
+	background-color: orangered;
+	cursor: pointer;
+}
+.sub-description {
+	border-radius: 1rem;
+	background-color: rgb(73, 73, 73);
+	color: white;
+	padding: 1rem;
+}
+.colors-def {
+	--primary: orangered;
+	--secondary: grey;
+	--background: #222222;
+	--backgroundlighter: rgb(63, 62, 62);
+	--mainwhite: #ffffff;
+}
+</style>
--- vue/src/pages/Subs.vue
+++ vue/src/pages/Subs.vue
@@ -1,11 +1,46 @@
 <template>
-	<div>Subs Subs Subs</div>
+	<div class="subs-container">
+		<h1 class="subs-header">Subs</h1>
+		<!-- TODO ajouter l'option de créer un Sub -->
+		<div v-for="(sub, index) in subsData" :key="index">
+			<SubCard :subData="sub" :subId="Number(index)" />
+		</div>
+	</div>
 </template>
 
 <script>
+import { getSubs } from "../api/subAPI.js";
+import SubCard from "../components/subs/SubCard.vue";
+
 export default {
 	name: "Subs",
+	components: { SubCard },
+	data: () => {
+		return {
+			subsData: {},
+		};
+	},
+	mounted() {
+		this.getSubsData();
+	},
+	methods: {
+		async getSubsData() {
+			this.subsData = await getSubs();
+		},
+	},
 };
 </script>
 
-<style></style>
+<style scoped>
+.subs-header {
+	color: var(--mainwhite);
+}
+.subs-container {
+	margin-top: 5rem;
+	margin-bottom: 7rem;
+	display: flex;
+	flex-direction: column;
+	gap: 2rem;
+	align-items: center;
+}
+</style>
