a8423cc65e87f07b503ebcad465dbdeaa3a0751d
Thomas Mondor <thomas.mondor.1@ulaval.ca>
2022-04-04 19:34:18 -0400

    remove header link to /sub (subs now at /sub/:subId and accessible from Subs page : '/')

--- vue/src/components/common/Header.vue
+++ vue/src/components/common/Header.vue
@@ -1,46 +1,37 @@
 <template>
-    <div class="hp-header">
-        <div class="hp-logo-container">
-            <img
-                src="../../../public/DitRed-logo.png"
-                class="hp-ditred-logo"
-                alt="ditred-logo"
-            />
-            <div class="hp-ditred-text">ditred</div>
-        </div>
-        <div class="hp-display-links">
-            <router-link to="/"
-                ><font-awesome-icon
-                    class="hp-header-icon"
-                    icon="fa-solid fa-list-ul"
-            /></router-link>
-            <router-link to="/user"
-                ><font-awesome-icon
-                    class="hp-header-icon"
-                    icon="fa-solid fa-user"
-            /></router-link>
-            <router-link to="/conversations"
-                ><font-awesome-icon
-                    class="hp-header-icon"
-                    icon="fa-solid fa-comment-dots"
-            /></router-link>
-            <router-link to="/sub"
-                ><font-awesome-icon
-                    class="hp-header-icon"
-                    icon="fa-solid fa-users"
-            /></router-link>
-            <router-link to="/sub-post"
-                ><font-awesome-icon
-                    class="hp-header-icon"
-                    icon="fa-solid fa-chalkboard-user"
-            /></router-link>
-        </div>
-    </div>
+	<div class="hp-header">
+		<div class="hp-logo-container">
+			<img
+				src="../../../public/DitRed-logo.png"
+				class="hp-ditred-logo"
+				alt="ditred-logo"
+			/>
+			<div class="hp-ditred-text">ditred</div>
+		</div>
+		<div class="hp-display-links">
+			<router-link to="/"
+				><font-awesome-icon class="hp-header-icon" icon="fa-solid fa-list-ul"
+			/></router-link>
+			<router-link to="/user"
+				><font-awesome-icon class="hp-header-icon" icon="fa-solid fa-user"
+			/></router-link>
+			<router-link to="/conversations"
+				><font-awesome-icon
+					class="hp-header-icon"
+					icon="fa-solid fa-comment-dots"
+			/></router-link>
+			<router-link to="/sub-post"
+				><font-awesome-icon
+					class="hp-header-icon"
+					icon="fa-solid fa-chalkboard-user"
+			/></router-link>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-    name: "Header",
+	name: "Header",
 };
 </script>
 
@@ -48,53 +39,53 @@ export default {
 @import url("https://fonts.googleapis.com/css2?family=Varela+Round&display=swap");
 
 .hp-display-links {
-    display: flex;
-    align-items: center;
-    gap: 1rem;
+	display: flex;
+	align-items: center;
+	gap: 1rem;
 }
 .hp-header {
-    display: flex;
-    align-items: center;
-    gap: 2vw;
+	display: flex;
+	align-items: center;
+	gap: 2vw;
 
-    position: sticky;
-    top: 0;
-    left: 0;
-    width: 100%;
+	position: sticky;
+	top: 0;
+	left: 0;
+	width: 100%;
 
-    height: 3.5rem;
+	height: 3.5rem;
 
-    background-color: var(--background);
+	background-color: var(--background);
 
-    z-index: 1000;
+	z-index: 1000;
 }
 
 .hp-header-icon {
-    height: 1.3rem;
-    color: var(--mainwhite);
+	height: 1.3rem;
+	color: var(--mainwhite);
 }
 
 .hp-header-icon:hover {
-    color: var(--primary);
+	color: var(--primary);
 }
 
 .hp-logo-container {
-    margin-left: 1vw;
-    display: flex;
-    gap: 0.3rem;
-    align-items: center;
-    color: var(--mainwhite);
+	margin-left: 1vw;
+	display: flex;
+	gap: 0.3rem;
+	align-items: center;
+	color: var(--mainwhite);
 
-    height: 100%;
+	height: 100%;
 }
 
 .hp-ditred-text {
-    font-family: "Varela Round", sans-serif;
-    font-size: 1.3rem;
-    font-weight: bold;
+	font-family: "Varela Round", sans-serif;
+	font-size: 1.3rem;
+	font-weight: bold;
 }
 
 .hp-ditred-logo {
-    height: 2rem;
+	height: 2rem;
 }
 </style>
