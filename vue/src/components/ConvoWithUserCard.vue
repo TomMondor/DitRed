f03fc2abbe1ade19eaf8614ff502acc41e1e615d
Thomas Mondor <thomas.mondor.1@ulaval.ca>
2022-04-13 19:39:16 -0400

    remove comment in html

--- vue/src/components/ConvoWithUserCard.vue
+++ vue/src/components/ConvoWithUserCard.vue
@@ -1,71 +1,70 @@
 <template>
-    <router-link
-        :to="{ name: 'Convo', params: { userId: this.userId } }"
-        class="router-link"
-    >
-        <!-- role="button" @click="redirectToConvo()" -->
-        <div class="convo-container">
-            <img
-                src="../../public/DitRed-logo.png"
-                class="ditred-logo"
-                alt="ditred-logo"
-            />
-            <div class="username">{{ username }}</div>
-        </div>
-    </router-link>
+	<router-link
+		:to="{ name: 'Convo', params: { userId: this.userId } }"
+		class="router-link"
+	>
+		<div class="convo-container">
+			<img
+				src="../../public/DitRed-logo.png"
+				class="ditred-logo"
+				alt="ditred-logo"
+			/>
+			<div class="username">{{ username }}</div>
+		</div>
+	</router-link>
 </template>
 
 <script>
 export default {
-    name: "ConvoWithUserCard",
-    props: ["userId", "username"],
-    methods: {
-        async redirectToConvo() {
-            this.$router.push("/conversations/" + this.userId);
-        },
-    },
+	name: "ConvoWithUserCard",
+	props: ["userId", "username"],
+	methods: {
+		async redirectToConvo() {
+			this.$router.push("/conversations/" + this.userId);
+		},
+	},
 };
 </script>
 
 <style scoped>
 .router-link {
-    text-decoration: none;
+	text-decoration: none;
 }
 
 .convo-container {
-    display: flex;
-    flex-direction: row;
-    width: 50vw;
-    height: fit-content;
-    padding: 2rem;
+	display: flex;
+	flex-direction: row;
+	width: 50vw;
+	height: fit-content;
+	padding: 2rem;
 
-    align-items: center;
-    justify-content: center;
-    gap: 2rem;
+	align-items: center;
+	justify-content: center;
+	gap: 2rem;
 
-    background-color: var(--background);
+	background-color: var(--background);
 
-    border-radius: 3rem;
+	border-radius: 3rem;
 
-    color: var(--mainwhite);
+	color: var(--mainwhite);
 }
 
 .convo-container:hover {
-    background-color: var(--primary);
-    cursor: pointer;
+	background-color: var(--primary);
+	cursor: pointer;
 }
 
 .ditred-logo {
-    width: 10vh;
-    height: 10vh;
+	width: 10vh;
+	height: 10vh;
 }
 
 .username {
-    font-size: 3rem;
-    font-weight: bold;
-    flex-grow: 1;
-    flex-wrap: wrap;
-    word-break: break-all;
-    text-align: center;
+	font-size: 3rem;
+	font-weight: bold;
+	flex-grow: 1;
+	flex-wrap: wrap;
+	word-break: break-all;
+	text-align: center;
 }
 </style>
