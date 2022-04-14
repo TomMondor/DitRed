19bfe4df7d094a266ce33ea9c1e07677ae7983b4
Thomas Mondor <thomas.mondor.1@ulaval.ca>
2022-04-13 20:56:24 -0400

    update Score component to prevent click when not logged in

--- vue/src/components/Score.vue
+++ vue/src/components/Score.vue
@@ -1,93 +1,106 @@
 <template>
-    <div :class="[isSmall ? 'score-container-small' : 'score-container']">
-        <font-awesome-icon
-            icon="fa-solid fa-arrow-up"
-            class="arrow-up"
-            :class="[isSmall ? 'arrow-small' : 'arrow']"
-            @click.stop="upvote"
-        />
-        <div class="score-text" :class="[score < 0 ? 'center-when-neg' : '']">
-            {{ score }}
-        </div>
-        <font-awesome-icon
-            icon="fa-solid fa-arrow-down"
-            class="arrow-down"
-            :class="[isSmall ? 'arrow-small' : 'arrow']"
-            @click.stop="downvote"
-        />
-    </div>
+	<div :class="[isSmall ? 'score-container-small' : 'score-container']">
+		<font-awesome-icon
+			icon="fa-solid fa-arrow-up"
+			:class="[isSmall ? 'arrow-small' : 'arrow', connected ? 'arrow-up' : '']"
+			@click.stop="upvote"
+		/>
+		<div class="score-text" :class="[score < 0 ? 'center-when-neg' : '']">
+			{{ score }}
+		</div>
+		<font-awesome-icon
+			icon="fa-solid fa-arrow-down"
+			:class="[
+				isSmall ? 'arrow-small' : 'arrow',
+				connected ? 'arrow-down' : '',
+			]"
+			@click.stop="downvote"
+		/>
+	</div>
 </template>
 
 <script>
+import { validateCookies } from "../api/loginAPI.js";
+
 export default {
-    name: "Score",
-    props: ["score", "isSmall"],
-    methods: {
-        upvote() {
-            this.$parent.$emit("upvote");
-        },
-        downvote() {
-            this.$parent.$emit("downvote");
-        },
-    },
+	name: "Score",
+	props: ["score", "isSmall"],
+	data: function () {
+		return {
+			connected: false,
+		};
+	},
+	async mounted() {
+		if (await validateCookies()) {
+			this.connected = true;
+		} else {
+			this.connected = false;
+		}
+	},
+	methods: {
+		upvote() {
+			if (this.connected) {
+				this.$parent.$emit("upvote");
+			}
+		},
+		downvote() {
+			if (this.connected) {
+				this.$parent.$emit("downvote");
+			}
+		},
+	},
 };
 </script>
 
 <style scoped>
 .score-container {
-    display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-    width: fit-content;
-    height: fit-content;
-    padding: 1rem;
+	display: flex;
+	flex-direction: column;
+	gap: 0.5rem;
+	width: fit-content;
+	height: fit-content;
+	padding: 1rem;
 
-    justify-content: center;
+	justify-content: center;
 }
 
 .score-container-small {
-    display: flex;
-    flex-direction: row;
-    gap: 0.5rem;
-    width: fit-content;
-    height: fit-content;
-    justify-content: center;
+	display: flex;
+	flex-direction: row;
+	gap: 0.5rem;
+	width: fit-content;
+	height: fit-content;
+	justify-content: center;
 }
 
 .score-text {
-    color: white;
-    text-align: center;
+	color: white;
+	text-align: center;
 }
 
 .center-when-neg {
-    text-align: left;
+	text-align: left;
 }
 
 .arrow {
-    color: grey;
-    height: 1.5rem;
-    width: 1.5rem;
+	color: grey;
+	height: 1.5rem;
+	width: 1.5rem;
 }
 
 .arrow-small {
-    color: grey;
-    height: 1rem;
-    width: 1rem;
-}
-
-.arrow:hover {
-    cursor: pointer;
-}
-
-.arrow-small:hover {
-    cursor: pointer;
+	color: grey;
+	height: 1rem;
+	width: 1rem;
 }
 
 .arrow-down:hover {
-    color: darkblue;
+	color: darkblue;
+	cursor: pointer;
 }
 
 .arrow-up:hover {
-    color: red;
+	color: red;
+	cursor: pointer;
 }
 </style>
