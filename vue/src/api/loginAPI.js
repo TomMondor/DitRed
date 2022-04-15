e181ea0899f2f04af8cdded566689cccd93a530b
Thomas Mondor <thomas.mondor.1@ulaval.ca>
2022-04-15 13:35:59 -0400

    move import to top of file

--- vue/src/api/loginAPI.js
+++ vue/src/api/loginAPI.js
@@ -1,45 +1,45 @@
+import Cookies from "js-cookie";
+
 const BASE_URL = "http://localhost:5000";
 
 export const login = async function (username, password) {
-    const response = await fetch(`${BASE_URL}/login`, {
-        method: "post",
-        body: JSON.stringify({
-            username: username,
-            password: password,
-        }),
-        headers: new Headers({
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        }),
-    });
+	const response = await fetch(`${BASE_URL}/login`, {
+		method: "post",
+		body: JSON.stringify({
+			username: username,
+			password: password,
+		}),
+		headers: new Headers({
+			Accept: "application/json",
+			"Content-Type": "application/json",
+		}),
+	});
 
-    if (response.status != 200) {
-        throw new Error("Username/password combination is invalid");
-    }
+	if (response.status != 200) {
+		throw new Error("Username/password combination is invalid");
+	}
 
-    const jsonResponse = await response.json();
-    return jsonResponse;
+	const jsonResponse = await response.json();
+	return jsonResponse;
 };
 
-import Cookies from "js-cookie";
-
 export const validateCookies = async function () {
-    const userId = Cookies.get("userId");
-    const loginToken = Cookies.get("loginToken");
+	const userId = Cookies.get("userId");
+	const loginToken = Cookies.get("loginToken");
 
-    const response = await fetch(`${BASE_URL}/validate`, {
-        method: "get",
-        headers: new Headers({
-            userId: userId,
-            loginToken: loginToken,
-        }),
-    });
+	const response = await fetch(`${BASE_URL}/validate`, {
+		method: "get",
+		headers: new Headers({
+			userId: userId,
+			loginToken: loginToken,
+		}),
+	});
 
-    if (response.status != 200) {
-        throw new Error("Could not validate cookies");
-    }
+	if (response.status != 200) {
+		throw new Error("Could not validate cookies");
+	}
 
-    const jsonResponse = await response.json();
+	const jsonResponse = await response.json();
 
-    return jsonResponse["valid"];
+	return jsonResponse["valid"];
 };
